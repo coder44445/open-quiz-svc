b32ae72d7f35990f41fe2b618a5b82f05fdde76b
coder44445 <yasheshchalla@gmail.com>
2026-07-08 00:20:32 +0530

    chore(alembic): create initial schema migration

---
+++ app/infrastructure/database/models/__init__.py
@@ -0,0 +1,13 @@
+from app.infrastructure.database.models.match import Match
+from app.infrastructure.database.models.player import Player
+from app.infrastructure.database.models.match_player import MatchPlayer
+from app.infrastructure.database.models.answer import Answer
+from app.infrastructure.database.models.question import Question
+
+__all__ = [
+    "Match",
+    "Player",
+    "MatchPlayer",
+    "Answer",
+    "Question"
+]
